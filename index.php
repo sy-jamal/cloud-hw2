b73ceb34a73c8216b23451f1f596c2104cf176a2
sy-jamal <syed.jamal.1@stonybrook.edu>
2019-02-17 08:04:58 -0500

    updated php file

--- index.php
+++ index.php
@@ -5,20 +5,19 @@
 	</head>
 	<body>
 		<?php
-			if(isset($_POST['uname'])){
-				echo "<h1>$_POST['uname']	</h1>"
+			if(isset($_POST['name'])){
+				echo "<h1>$_POST['name']	</h1>";
 			}
 			else
 			{
 		?>
 				<div class = "submission form">		
 				<form action="" method= "POST">
-					<input type="text" name="uname" placeholder="Please enter your name..">
+					<input type="text" name="name" placeholder="Please enter your name..">
 					<input type="submit" valuue= "Submit">		
 				</form>
 			</div>
 		<?php	} ?>
-
-			
+					
 	</body>
 </html>
